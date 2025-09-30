140cd4c5c415f2155079c3325d95b37eae3bbd11
WodaMineralna <raknieborak65@gmail.com>
2025-09-30 23:53:00 +0200

    feat(auth): apply isAuthed middleware to protected routes
    - Ensure only authenticated users can access restricted pages

--- routes/admin.js
+++ routes/admin.js
@@ -4,26 +4,45 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
+const isAuthed = require("../middleware/isAuthed");
 const catchErrAsync = require("../utils/catchErrAsync");
 
 // /admin/products => GET
-router.get("/products", catchErrAsync(adminController.getProductsPage));
+router.get(
+  "/products",
+  isAuthed,
+  catchErrAsync(adminController.getProductsPage)
+);
 
 // /admin/add-product => GET
-router.get("/add-product", catchErrAsync(adminController.getAddProduct));
+router.get(
+  "/add-product",
+  isAuthed,
+  catchErrAsync(adminController.getAddProduct)
+);
 
 // /admin/add-product => POST
-router.post("/add-product", catchErrAsync(adminController.postAddProduct));
+router.post(
+  "/add-product",
+  isAuthed,
+  catchErrAsync(adminController.postAddProduct)
+);
 
 router.get(
   "/edit-product/:productId",
+  isAuthed,
   catchErrAsync(adminController.getEditProduct)
 );
 
-router.post("/edit-product", catchErrAsync(adminController.postEditProduct));
+router.post(
+  "/edit-product",
+  isAuthed,
+  catchErrAsync(adminController.postEditProduct)
+);
 
 router.post(
   "/delete/:productId",
+  isAuthed,
   catchErrAsync(adminController.postDeleteProduct)
 );
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,7 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
+const isAuthed = require("../middleware/isAuthed");
 const catchErrAsync = require("../utils/catchErrAsync");
 
 router.get("/", catchErrAsync(shopController.getIndex));
@@ -12,17 +13,22 @@ router.get("/products", catchErrAsync(shopController.getProductsPage));
 
 router.get("/products/:id", catchErrAsync(shopController.getProduct));
 
-router.get("/cart", catchErrAsync(shopController.getCart));
+router.get("/cart", isAuthed, catchErrAsync(shopController.getCart));
 
-router.post("/cart", catchErrAsync(shopController.postCart));
+router.post("/cart", isAuthed, catchErrAsync(shopController.postCart));
 
 router.post(
   "/cart/delete/:productId",
+  isAuthed,
   catchErrAsync(shopController.postDeleteCart)
 );
 
-router.get("/orders", catchErrAsync(shopController.getOrders));
+router.get("/orders", isAuthed, catchErrAsync(shopController.getOrders));
 
-router.post("/orders/create", catchErrAsync(shopController.postOrder));
+router.post(
+  "/orders/create",
+  isAuthed,
+  catchErrAsync(shopController.postOrder)
+);
 
 module.exports = router;
