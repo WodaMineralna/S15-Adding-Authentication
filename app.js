4b41d70aeaea1184b72cbb3eab92cdbe273a663a
WodaMineralna <raknieborak65@gmail.com>
2025-10-01 20:19:47 +0200

    refactor(app): outsource configuration into dedicated modules
    - Moved session setup to `config/session.js`
    - Moved CSRF setup to `config/csrf.js`
    - Grouped route imports in `routes/index.js`
    - Modified `middleware/attachUser.js` to use `catchErrAsync` in middleware file instead of app.js

--- app.js
+++ app.js
@@ -1,58 +1,33 @@
 const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const csrf = require("csurf");
 require("dotenv").config();
 
-const required = require("./utils/requireEnvVar");
-const catchErrAsync = require("./utils/catchErrAsync");
+const sessionMiddleware = require("./config/session");
+const csrfProtection = require("./config/csrf");
+const routes = require("./routes/index");
 
 const attachUser = require("./middleware/attachUser");
 const attachLocals = require("./middleware/attachLocals");
-
 const errorController = require("./controllers/error");
 
-const { mongoConnect, getMongoDB_URI } = require("./src/db/database");
+const { mongoConnect } = require("./src/db/database");
 
 const app = express();
-const csrfProtection = csrf();
-
-const MongoDB_URI = getMongoDB_URI();
-const store = new MongoDBStore({
-  uri: MongoDB_URI,
-  collection: "sessions",
-});
-
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-app.use(
-  session({
-    secret: required("SESSION_HASH"),
-    resave: false,
-    saveUninitialized: false,
-    store,
-    // cookie: {}
-  })
-);
+
+app.use(sessionMiddleware());
 app.use(csrfProtection);
 
-app.use(catchErrAsync(attachUser)); // ^ attaches user found by User.findById(req.session.user._id) to req.user
+app.use(attachUser); // ^ attaches user found by User.findById(req.session.user._id) to req.user
 app.use(attachLocals); // ^ automatically attaches res.locals.loggedIn && .csrfToken to currently rendered views
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-
+app.use(routes);
 app.use(errorController.get404);
 app.use(errorController.getErrorPage);
 
