2c20c3a8feee5c7bda0e3257441906895e426e9b
WodaMineralna <raknieborak65@gmail.com>
2025-10-01 20:23:28 +0200

    refactor(app): create server.js as entry point for app startup
    - Add server.js to load `dotenv`, connect to MongoDB and execute `app.listen(3000)`
    - Moved server creation logic from app.js to server.js
    - Updated package.json scripts to execute server.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
 
 const sessionMiddleware = require("./config/session");
 const csrfProtection = require("./config/csrf");
@@ -10,8 +9,6 @@ const routes = require("./routes/index");
 const attachUser = require("./middleware/attachUser");
 const attachLocals = require("./middleware/attachLocals");
 const errorController = require("./controllers/error");
-
-const { mongoConnect } = require("./src/db/database");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -28,10 +25,8 @@ app.use(attachUser); // ^ attaches user found by User.findById(req.session.user.
 app.use(attachLocals); // ^ automatically attaches res.locals.loggedIn && .csrfToken to currently rendered views
 
 app.use(routes);
+
 app.use(errorController.get404);
 app.use(errorController.getErrorPage);
 
-// ^ setting up MongoDB connection
-mongoConnect(() => {
-  app.listen(3000);
-});
+module.exports = app;
