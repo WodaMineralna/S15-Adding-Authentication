4384cf645d1002664ef4952069ccc3f052a176c7
WodaMineralna <raknieborak65@gmail.com>
2025-10-02 18:46:41 +0200

    test(db): remove legacy example user lookup

--- scripts/test-db.cjs
+++ scripts/test-db.cjs
@@ -1,28 +1,18 @@
 const { mongoConnect, close } = require("../src/db/database");
 
-const required = require("../utils/requireEnvVar")
-
 const User = require("../models/user");
 const Product = require("../models/product");
-
-const USER_ID = required("MONGODB_EXAMPLE_USER_ID");
 
 (async () => {
   try {
     await mongoConnect();
 
-    const rawUser = await User.find({ _id: USER_ID });
-    if (!rawUser) {
-      throw new Error(`Could not find user!`);
-    }
-
     const productData = await Product.fetchAll();
     const userData = await User.find();
 
-    console.log("===== DB connection OK =====");
+    console.log("===== DB connection OK =====\n");
     console.log("--- Product data: ---", productData);
-    console.log("--- \"Logged-in\" user data: ---", rawUser);
-    console.log("--- User data: ---", userData);
+    console.log("\n--- User data: ---", userData);
   } catch (error) {
     console.error("===== DB connection FAILED =====");
     console.error(error.message);
